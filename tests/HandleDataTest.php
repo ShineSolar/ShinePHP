2be5755740df28d6be52e9cb4cdb3bf3ef0a0c97
Adam McGurk <mcgurk@mcgurkconsulting.com>
2019-07-13 12:06:35 -0600

    Cleanup commit

--- tests/HandleDataTest.php
+++ tests/HandleDataTest.php
@@ -5,9 +5,42 @@ use PHPUnit\Framework\TestCase;
 
 // Remember, requires are from the root in tests
 require 'src/ShinePHP/HandleData.php';
-use ShinePHP\{HandleData};
+use ShinePHP\{HandleData, IpValidator};
 
 final class HandleDataTest extends TestCase {
+
+    public function testValidIpAddresses(): void {
+
+        $reg_valid = new IpValidator('192.168.0.1');
+        $this->assertEquals('192.168.0.1', $reg_valid->validate_general_ip());
+
+        $priv_addy = new IpValidator('FC80:0000:0000:0000:903A:1C1A:E802:11E4');
+
+        $this->assertEquals('FC80:0000:0000:0000:903A:1C1A:E802:11E4', $priv_addy->validate_private_ipv6());
+    }
+
+    public function testInvalidIpAddresses(): void {
+        $reg_invalid = new IpValidator('256.71.83.1');
+        $this->assertFalse($reg_invalid->validate_general_ip());
+
+        $invalid_ipv4 = new IpValidator('FC80:0000:0000:0000:903A:1C1A:E802:11E4');
+        $this->assertFalse($invalid_ipv4->validate_general_ipv4());
+
+        $invalid_public_ipv4_reserved = new IpValidator('127.0.0.1');
+        $this->assertFalse($invalid_public_ipv4_reserved->validate_public_ipv4());
+
+        $invalid_public_ipv4_private = new IpValidator('192.168.0.1');
+        $this->assertFalse($invalid_public_ipv4_private->validate_public_ipv4());
+
+        $invalid_private_ipv4_reserved = new IpValidator('127.0.0.1');
+        $this->assertFalse($invalid_private_ipv4_reserved->validate_private_ipv4());
+
+        $invalid_private_ipv4_public = new IpValidator('207.124.51.1');
+        $this->assertFalse($invalid_private_ipv4_public->validate_private_ipv4());
+
+        $invalid_subnet = new IpValidator('192.168.0.1');
+        $this->assertFalse($invalid_subnet->validate_subnet_mask());
+    }
 
     public function testingValidData() : void {
 
@@ -155,9 +188,6 @@ final class HandleDataTest extends TestCase {
 
     // Testing empty url
     public function testEmptyInvalidUrl() : void { $this->assertFalse(HandleData::url('')); }
-
-    // Testing invalid ip address (regular 4 octect numerals)
-    public function testInvalidIpAddress() : void { $this->assertFalse(HandleData::ip('256.101.7.10')); }
 
     // Testing invalid float passing 0
     public function testInvalidFloatPassingZero() : void { $this->assertFalse(HandleData::float(0)); }
