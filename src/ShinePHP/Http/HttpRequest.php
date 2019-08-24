7e5587c1cf3e7c81d977b8da4331baad9dfd4f93
Adam McGurk <mcgurk@mcgurkconsulting.com>
2019-08-24 08:59:22 -0600

    worked on some small syntax

--- src/ShinePHP/Http/HttpRequest.php
+++ src/ShinePHP/Http/HttpRequest.php
@@ -16,40 +16,40 @@ final class HttpRequest {
 		// setting the class vars
 		$verified_method = self::verify_method($method);
 
-		$this->curl_handle = curl_init(self::build_url($url, $query_params));
+		$this->curl_handle = \curl_init(self::build_url($url, $query_params));
 
 		if ($verified_method === 'POST') {
-			curl_setopt($this->curl_handle, CURLOPT_POST, 1);
+			\curl_setopt($this->curl_handle, CURLOPT_POST, 1);
 		} else if ($verified_method !== 'GET') {
-			curl_setopt($this->curl_handle, CURLOPT_CUSTOMREQUEST, $verified_method);
+			\curl_setopt($this->curl_handle, CURLOPT_CUSTOMREQUEST, $verified_method);
 		}
 
-		curl_setopt($this->curl_handle, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($this->curl_handle, CURLOPT_FAILONERROR, true);
-		curl_setopt($this->curl_handle, CURLOPT_DEFAULT_PROTOCOL, 'https');
+		\curl_setopt($this->curl_handle, CURLOPT_RETURNTRANSFER, true);
+		\curl_setopt($this->curl_handle, CURLOPT_FAILONERROR, true);
+		\curl_setopt($this->curl_handle, CURLOPT_DEFAULT_PROTOCOL, 'https');
 
 	}
 
 	public function set_request_headers(array $headers): void {
-		curl_setopt($this->curl_handle, CURLOPT_HTTPHEADER, $headers);
+		\curl_setopt($this->curl_handle, CURLOPT_HTTPHEADER, $headers);
 	}
 
 	public function do_basic_auth(string $username, string $password): void {
-		curl_setopt($this->curl_handle, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-		curl_setopt($this->curl_handle, CURLOPT_USERPWD, $username.':'.$password);
+		\curl_setopt($this->curl_handle, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		\curl_setopt($this->curl_handle, CURLOPT_USERPWD, $username.':'.$password);
 	}
 
 	public function set_oauth_bearer_token(string $token): void {
-		curl_setopt($this->curl_handle, CURLOPT_XOAUTH2_BEARER, $token);
+		\curl_setopt($this->curl_handle, CURLOPT_XOAUTH2_BEARER, $token);
 	}
 
 	public function send($post_data = ''): ?string {
 
 		if (!empty($post_data)) {
-			curl_setopt($this->curl_handle, CURLOPT_POSTFIELDS, $post_data);
+			\curl_setopt($this->curl_handle, CURLOPT_POSTFIELDS, $post_data);
 		}
 
-		$curl_response = curl_exec($this->curl_handle);
+		$curl_response = \curl_exec($this->curl_handle);
 
 		if ($curl_response === false) {
 			throw new HttpException('HTTP Error: '.curl_error($this->curl_handle));
@@ -89,7 +89,7 @@ final class HttpRequest {
 
 		$parsed_url = \parse_url($url);
 
-		return (isset($parsed_url['query']) ? $url.'&'.http_build_query($query_params) : $url.'?'.http_build_query($query_params));
+		return (isset($parsed_url['query']) ? $url.'&'.\http_build_query($query_params) : $url.'?'.\http_build_query($query_params));
 
 	}
 
--- src/ShinePHP/Http/IncomingRequest.php
+++ src/ShinePHP/Http/IncomingRequest.php
@@ -20,7 +20,7 @@ final class IncomingRequest {
 	public function get_custom_header_value(string $header_value): ?string {
 
 		// replacing dashes with underscores
-		$safe_value = str_replace('-', '_', $header_value);
+		$safe_value = \strtoupper(\str_replace('-', '_', $header_value));
 
 		// returning the value or null
 		return (isset($_SERVER['HTTP_'.$safe_value]) ? $_SERVER['HTTP_'.$safe_value] : null);
@@ -30,22 +30,22 @@ final class IncomingRequest {
 	public function retrieve_json_input(string $retrieve_url = 'php://input'): array {
 
 		// decode JSON array
-		$decoded_json = json_decode(file_get_contents($retrieve_url), true);
+		$decoded_json = \json_decode(file_get_contents($retrieve_url), true);
 
 		// return an empty array if there was no json to return, otherwise return the decoded json
-		return (is_null($decoded_json) ? array() : $decoded_json);
+		return (\is_null($decoded_json) ? array() : $decoded_json);
 
 	}
 
 	public function require_input_data(?array $input_data, array $field_names_to_validate): array {
 
-		if (empty($input_data) || is_null($input_data)) {
+		if (empty($input_data) || \is_null($input_data)) {
 			throw new \Exception('Input cannot be empty');
 		}
 
 		foreach ($required_input_names as $name) {
 
-			if (array_key_exists($name, $input_data) === false) {
+			if (\array_key_exists($name, $input_data) === false) {
 				throw new \Exception($name.' cannot be omitted');
 			}
 			
