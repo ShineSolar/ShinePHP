6a020f2214015b072963ce3c622fdd039af97336
Adam McGurk <mcgurk@mcgurkconsulting.com>
2019-08-26 15:55:43 -0600

    Updated some tests

--- tests/Integration/Database/CrudTest.php
+++ tests/Integration/Database/CrudTest.php
@@ -9,6 +9,20 @@ use ShinePHP\Database\{Crud, CrudException};
 
 final class CrudTest extends TestCase {
 
+	public function testEnvNotEnoughValues(): void {
+		$this->expectException(CrudException::class);
+		putenv('DB_NAME=test_db');
+		Crud::get_from_environment();
+	}
+
+	public function testEnv(): void {
+		putenv('DB_NAME=test_db');
+		putenv('DB_USERNAME=your_mysql_client');
+		putenv('DB_PASSWORD=your_mysql_password');
+		$env_variables = Crud::get_from_environment();
+		$this->assertEquals('your_mysql_client', $env_variables['username']);
+	}
+
 	public function testIniNotExists(): void {
 		$this->expectException(CrudException::class);
 		Crud::get_from_ini_file('../../not-existing.ini');
