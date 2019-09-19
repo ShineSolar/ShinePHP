12a78509fb2785ba60b4752318bddd22d7532b05
Adam McGurk <mcgurk@mcgurkconsulting.com>
2019-09-19 14:18:28 -0600

    Added a type to the throw on error parameter

--- src/ShinePHP/Http/HttpRequest.php
+++ src/ShinePHP/Http/HttpRequest.php
@@ -43,7 +43,7 @@ final class HttpRequest {
 		\curl_setopt($this->curl_handle, CURLOPT_XOAUTH2_BEARER, $token);
 	}
 
-	public function send($post_data = '', $throw_on_error = false): ?string {
+	public function send($post_data = '', bool $throw_on_error = false): ?string {
 
 		if (!empty($post_data)) {
 			\curl_setopt($this->curl_handle, CURLOPT_POSTFIELDS, $post_data);
